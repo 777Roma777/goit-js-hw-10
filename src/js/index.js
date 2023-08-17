1e5de808f83879ac762cfb214d19d7a033041baf
777Roma777 <nanoeagle911@gmail.com>
2023-08-17 12:15:37 +0300

    test

--- src/js/index.js
+++ src/js/index.js
@@ -12,50 +12,49 @@ refs.selectBreed.addEventListener('change', getInformationAboutCat);
 fetchAndRenderBreeds();
 
 function fetchAndRenderBreeds() {
-    refs.selectBreed.style.display = 'none';
-    refs.errorElement.style.display = 'none';
-    fetchBreeds()
-      .then(breeds => {
-        refs.selectBreed.innerHTML = renderBreedsSelect(breeds);
-        refs.selectBreed.style.display = 'block';
-      })
-      .catch(() => {
-        Notify.failure(refs.errorElement.textContent);
-      })
-      .finally(() => {
-        loaderElement.style.display = 'none';
-      });
-  }
+  refs.selectBreed.style.display = 'none';
+  refs.errorElement.style.display = 'none';
+  refs.loaderElement.style.display = 'block';
+
+  fetchBreeds()
+    .then(breeds => {
+      refs.selectBreed.innerHTML = renderBreedsSelect(breeds);
+      refs.selectBreed.style.display = 'block';
+    })
+    .catch(() => {
+      Notify.failure(refs.errorElement.textContent);
+    })
+    .finally(() => {
+      refs.loaderElement.style.display = 'none';
+    });
+}
 
 function getInformationAboutCat(event) {
-    refs.catInfo.innerHTML = '';
-    refs.catInfo.style.display = 'none';
-    refs.loaderElement.style.display = 'block';
-  
-    fetchCatByBreed(event.target.value)
-      .then(catInfo => {
-        console.log(catInfo[0].url);
-        let { name, description, temperament } = catInfo.breeds[0];
-        let { imageUrl } = catInfo.url;
-  
-        refs.catInfo.innerHTML = markupCatInformation({
-          name,
-          description,
-          temperament,
-          imageUrl,
-        });
-  
-        refs.catInfo.style.display = 'block';
-      })
-      .catch(() => {
-        Notify.failure(refs.errorElement.textContent);
-      })
-      .finally(() => {
-        refs.loaderElement.style.display = 'none';
+  refs.catInfo.innerHTML = '';
+  refs.catInfo.style.display = 'none';
+  refs.loaderElement.style.display = 'block';
+
+  fetchCatByBreed(event.target.value)
+    .then(catInfo => {
+      let { name, description, temperament } = catInfo.breeds[0];
+      let { url } = catInfo;
+
+      refs.catInfo.innerHTML = markupCatInformation({
+        name,
+        description,
+        temperament,
+        url,
       });
-  }
 
-
+      refs.catInfo.style.display = 'block';
+    })
+    .catch(() => {
+      Notify.failure(refs.errorElement.textContent);
+    })
+    .finally(() => {
+      refs.loaderElement.style.display = 'none';
+    });
+}
 
 function renderBreedsSelect(breeds) {
   return breeds
@@ -63,11 +62,9 @@ function renderBreedsSelect(breeds) {
     .join('');
 }
 
-
-
-function markupCatInformation({ name, description, temperament, imageUrl }) {
+function markupCatInformation({ name, description, temperament, url }) {
   return `
-    <img src="${imageUrl}" alt="${name}"/>
+    <img src="${url}" alt="${name}"/>
     <h2>${name}</h2>
     <h3>${description}</h3>
     <p>${temperament}</p>
